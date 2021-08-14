6c0c2ef1dc08af640bc86d6bd919760614a0b2cf
Findart TCC <findarttcc@gmail.com>
2021-08-14 12:25:46 +0000

    atualização 3

--- Findart/App.js
+++ Findart/App.js
@@ -4,6 +4,7 @@ import { StyleSheet, StatusBar,} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
 import {createBottomTabNavigator} from '@react-navigation/bottom-tabs';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Colecao from './src/views/Colecao/Colecao';
 import Inicial from './src/views/Inicial/Inicial';
 import Item from './src/views/Item/Item';
@@ -11,50 +12,118 @@ import Perfil from './src/views/Perfil/Perfil';
 
 const Stack = createStackNavigator();
 const Tab = createBottomTabNavigator();
+const Drawer = createDrawerNavigator();
 
+function rotasDrawer(){
+  return (
+    <Drawer.Navigator initialRouteName='Inicial'>
+      <Drawer.Screen 
+      options={
+        {cardStyle: styles.screenInicial,
+        
+        headerStyle: {backgroundColor: '#14417b'},
+        headerTintColor: '#FFFFFF',
+        
+        }}
+      name="Inicial" component ={rotasStack}/>
+      <Drawer.Screen
+      options={
+        {cardStyle: styles.screenInicial,
+        
+        headerStyle: {backgroundColor: '#14417b'},
+        headerTintColor: '#FFFFFF',
+        
+        }}
+      name="Colecao" component ={Colecao}/>
+      <Drawer.Screen
+      options={
+        {cardStyle: styles.screenInicial,
+        
+        headerStyle: {backgroundColor: '#14417b'},
+        headerTintColor: '#FFFFFF',
+        
+        }}
+      name="Item" component ={Item}/>
+      <Drawer.Screen
+      options={
+        {cardStyle: styles.screenInicial,
+        
+        headerStyle: {backgroundColor: '#14417b'},
+        headerTintColor: '#FFFFFF',
+        
+        }}
+      name="Perfil" component ={Perfil}/>
+      
+    </Drawer.Navigator>
+  );
+}
 
 function rotasStack() {
   return (
     <Stack.Navigator>
 
-<Stack.Screen name="Inicial" 
+       
+        <Stack.Screen 
+        name="Inicial" 
         component={Inicial}
-        options={{
-          cardStyle: styles.screenInicial,
-          headerStyle: {backgroundColor: '14417b'},
-          headerTintColor: '#FFFFFF',
+        options={
+          {cardStyle: styles.screenInicial,
+          headerShown: false,
           
-        }}/>
-        <Stack.Screen name="Colecao"
-         component={Colecao}
-         options={{
-          cardStyle: styles.screenColecao,
-          headerStyle: {backgroundColor: '14417b'},
-          headerTintColor: '#FFFFFF',
-        }}/>
-        <Stack.Screen name="Item"
-         component={Item}
-         options={{
-          cardStyle: styles.screenItem,
-          headerStyle: {backgroundColor: '14417b'},
-          headerTintColor: '#FFFFFF',
-        }}/>
+          }
+        }
+      />
+        <Stack.Screen 
+        name="Colecao"
+        component={Colecao}
+        options={
+          { cardStyle: styles.screenColecao,
+            headerShown: false,
+          }
+        }
+      />
+        <Stack.Screen 
+        name="Item"
+        component={Item}
+        options={
+          { cardStyle: styles.screenItem,
+            headerShown: false,
+          }
+        }
+      />
 
     </Stack.Navigator>
-    ):
+    )
 }
 export default function App(){
   return(
     <NavigationContainer>
-    
+
       <Tab.Navigator
+        barStyle = {{backgroundColor :'#14417b'}}
         tabBarOptions={
+          
           {
+            style: {backgroundColor: '#14417b'},
             labelStyle: {fontSize: 24},
-          }
-        }>
-        <Tab.Screen name="Inicial" component={Inicial}/>
-        <Tab.Screen name="Perfil"  component={Perfil}/>
+            activeTintColor: '#14417b', 
+            
+          }}
+         
+       >
+        <Tab.Screen 
+        options ={{
+          headerShown: false,
+          
+        }}
+        
+        name="Inicial" component={rotasDrawer}/>
+        <Tab.Screen
+         options ={{
+          headerShown: false,
+          
+        }}
+        name="Perfil" component={Perfil}/>
       </Tab.Navigator>
 
     </NavigationContainer>
@@ -70,5 +139,4 @@ const styles = StyleSheet.create({
     screenItem: {
       backgroundColor: "#d1d1d1"
     },
-  }
-);
+});
