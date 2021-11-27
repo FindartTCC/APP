44ec72cc1dfbd823881471bb0a2ed98c007bd805
Findart TCC <findarttcc@gmail.com>
2021-11-27 03:08:37 +0000

    Explorar_27/11

--- Findart/src/views/Explorar/Explorar.js
+++ Findart/src/views/Explorar/Explorar.js
@@ -1,7 +1,8 @@
 import React from 'react';
-import {Text, View, TouchableOpacity, Image} from 'react-native';
+import {Text, View, TouchableOpacity, Image, ScrollView, TextInput} from 'react-native';
 import estiloExplorar from './estiloExplorar';
-import Exemplo from "../../imgs/exemplo.jpg"
+import Exemplo from "../../imgs/emicida.jpg"
+import ExemploP from "../../imgs/emi.jpeg"
 function Explorar ({navigation}) {
     const voltar = () => {
         navigation.goBack();
@@ -9,19 +10,89 @@ function Explorar ({navigation}) {
     return (
         <View style={estiloExplorar.container}>
             <View style={estiloExplorar.borda}>
-
+            <ScrollView 
+            
+            showsVerticalScrollIndicator={false}
+            >
                 <Text style={estiloExplorar.texto}>Explorar</Text>
                 <Text style={estiloExplorar.textocorrido}>Aqui estarão publicações variadas para encontrar diveros artistas diferentes</Text>
                 
+                <View style={estiloExplorar.container}>
                 <View style={estiloExplorar.ContainerNome}>
-                    <Text style={estiloExplorar.botaoTexto}>Zezé Di Cmargo</Text>
+                    
+                    <Image
+                style={estiloExplorar.imgP}
+                source={ExemploP}
+                />
+                
+                <Text style={estiloExplorar.textoNome}><a>Emicida </a><p>Há 5 min</p></Text>
+                
+                    
                 </View>
+                </View>
+
                 <View style={estiloExplorar.container}>
                 <Image
                 style={estiloExplorar.img}
                 source={Exemplo}
                 />
                 </View>
+
+                <View style={estiloExplorar.container}>
+                <View style={estiloExplorar.ContainerComenta}>
+                <TouchableOpacity style={estiloExplorar.botao} onPress={voltar}>
+                    <Text style={estiloExplorar.botaoTexto}>Curtir</Text>
+                </TouchableOpacity><TouchableOpacity style={estiloExplorar.botao} onPress={voltar}>
+                    <Text style={estiloExplorar.botaoTexto}>Comentar</Text>
+                </TouchableOpacity><TouchableOpacity style={estiloExplorar.botao} onPress={voltar}>
+                    <Text style={estiloExplorar.botaoTexto}>Compartilhar</Text>
+                </TouchableOpacity>
+                
+                </View>
+                </View>
+                
+                
+
+
+
+
+
+                <View style={estiloExplorar.container}>
+                <View style={estiloExplorar.ContainerNome}>
+                    
+                    <Image
+                style={estiloExplorar.imgP}
+                source={ExemploP}
+                />
+                
+                <Text style={estiloExplorar.textoNome}><a>Emicida </a><p>Há 5 min</p></Text>
+                
+                    
+                </View>
+                </View>
+
+                <View style={estiloExplorar.container}>
+                <Image
+                style={estiloExplorar.img}
+                source={Exemplo}
+                />
+                </View>
+
+                <View style={estiloExplorar.container}>
+                <View style={estiloExplorar.ContainerComenta}>
+                <TouchableOpacity style={estiloExplorar.botao} onPress={voltar}>
+                    <Text style={estiloExplorar.botaoTexto}>Curtir</Text>
+                </TouchableOpacity><TouchableOpacity style={estiloExplorar.botao} onPress={voltar}>
+                    <Text style={estiloExplorar.botaoTexto}>Comentar</Text>
+                </TouchableOpacity><TouchableOpacity style={estiloExplorar.botao} onPress={voltar}>
+                    <Text style={estiloExplorar.botaoTexto}>Compartilhar</Text>
+                </TouchableOpacity>
+                
+                </View>
+                </View>
+
+
+                </ScrollView> 
                 <TouchableOpacity style={estiloExplorar.botaoContainer} onPress={voltar}>
                     <Text style={estiloExplorar.botaoTexto}>Voltar</Text>
                 </TouchableOpacity>
--- Findart/src/views/Explorar/estiloExplorar.js
+++ Findart/src/views/Explorar/estiloExplorar.js
@@ -5,6 +5,7 @@ const estiloExplorar = StyleSheet.create({
         flex:1,
         justifyContent: 'center',
         alignItems: 'center',
+        
     },
     
     borda: {
@@ -20,6 +21,7 @@ const estiloExplorar = StyleSheet.create({
         textAlign: 'center',
         padding: 10
     },
+   
     textocorrido: {
         fontSize:20,
         color: '#420000',
@@ -41,31 +43,96 @@ const estiloExplorar = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    ContainerNome: {
-        marginTop: 20,
-        marginBottom: 20,
-        marginLeft: 40,
-        marginRight: 40,
-        borderRadius: 0,
-       padding:10,
-        height:40,
+    botao: {
+        marginTop: 10,
+        marginBottom: 10,
+        marginLeft: 5,
+        marginRight: 5,
+        //borderRadius: 5,
+        height:50,
         
-       
+        //borderWidth: 2,
+        font: 16,
+        //borderColor: '#D97941',
         backgroundColor: '#A61F2B',
         justifyContent: 'center',
         alignItems: 'center',
+    },
+    ContainerNome: {
+        display: 'flex',
+        borderRadius: 0,
+        padding:10,
+        paddingBottom:0 ,
+        height:80,
+        width: '80%',
+        backgroundColor: '#A61F2B',
+        justifyContent: 'left',
+        alignItems: 'left',
+        marginLeft:0 ,
+        flexDirection: "row",
+        borderRadius: 10
+    },
+    ContainerComenta: {
+        display: 'flex',
+        borderRadius: 0,
+        padding:0,
+        paddingBottom:0 ,
+        marginTop: 50,
+        height:60,
+        width: '80%',
+        backgroundColor: '#A61F2B',
+        justifyContent: 'center',
+        alignItems: 'center',
+        marginLeft:0 ,
+        flexDirection: "row",
+        borderRadius:10
+        
     },
     botaoTexto: {
         fontSize: 18,
         color: '#FFFFFF',
         fontWeight: 'bold'
     },
+    campo: {
+        backgroundColor: '#F2A74B',
+        fontSize: 18,
+        marginLeft:40,
+        marginRight:40,
+        marginTop:5,
+        borderRadius:5,
+        height: 40,
+        paddingLeft: 10,
+        color: "#420000"
+    },
+    textoNome: {
+        fontSize: 17,
+        color: '#FFFFFF',
+        fontWeight: 'bold',
+        textAlignVertical: 'auto'
+    },
+    p: {
+        marginBottom: 0,
+        marginTop: 0,
+        padding:0
+    },
     img: {
         justifyContent:'center',
         height: 300,
-        width: 300
-    }
-
+        width: '80%',
+        marginTop: 25,
+        
+    },
+    imgP: {
+    
+        height: 60,
+        width: 60,
+        marginRight:20,
+        marginBottom:0,
+        marginTop: 0,
+        paddingBottom:0,
+        paddingTop:0,
+        borderRadius: 10
+    } 
 })
 
 export default estiloExplorar;
